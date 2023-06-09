04576c30563535a4652330550bff89529ca80d59
Shane Pyall <shanepyall@gmail.com>
2023-06-09 13:19:38 +0100

    Included ability to add CSV file types

--- movies-app/main.py
+++ movies-app/main.py
@@ -1,7 +1,8 @@
 from movie_app import MovieApp
 from storage_json import StorageJson
+from storage_csv import StorageCsv
 
-amy = StorageJson('movies.json')
+amy = StorageCsv('movies.csv')
 jack = StorageJson('movies.json')
 movie_app = MovieApp(amy)
 movie_app.run()
--- movies-app/storage_csv.py
+++ movies-app/storage_csv.py
@@ -1,28 +1,32 @@
 from istorage import IStorage
-import json
+import csv
 import requests
 
 API_KEY = 'be04b8e5'
 DATA_LINK = f"http://www.omdbapi.com/?apikey={API_KEY}&t="
 
 
-class StorageJson(IStorage):
+class StorageCsv(IStorage):
     # Stores movies CRUD data in a CSV file.
     def __init__(self, file_path):
         self.file_path = file_path
 
     def list_movies(self):
         # Returns a string of all movies in database.
-        with open(self.file_path, "r") as json_movie_storage:
-            movies = json.loads(json_movie_storage.read())
+        with open(self.file_path, "r") as csv_movie_storage:
+            movies = csv.reader(csv_movie_storage)
 
-        list_of_movies_text = f"\n{len(movies)} movies in total\n"
-    
-        for movie in movies:
-            list_of_movies_text += f"\n{movie['Title']}: {movie['Rating']}, {movie['Year']}"
+        count = 0
+        list_of_movies_text = ''
+        for row in movies:
+            if count == 0:
+                continue
+            list_of_movies_text += f"\n{row['Title']}: {row['Rating']}, {row['Year']}"
+            count += 1
+        final = f"\n{count} movies in total\n" + list_of_movies_text
 
-        list_of_movies_text += "\n"
-        return list_of_movies_text
+        final += "\n"
+        return final
 
     def add_movie(self, title):
         # Adds user input (title) to search link and prints(for now) the expected.
@@ -30,7 +34,7 @@ class StorageJson(IStorage):
         parsed = res.json()
 
         with open(self.file_path, "r") as readable:
-            movies = json.loads(readable.read())
+            movies = csv.reader(readable)
 
         new_movie_dict = {"Title": parsed["Title"], "Rating": parsed["Ratings"][0]["Value"], "Year": parsed["Year"],
                           "Poster URL": parsed["Poster"]}
@@ -38,42 +42,47 @@ class StorageJson(IStorage):
         for movie in movies:
             if movie["Title"] == new_movie_dict["Title"]:
                 return f'Movie {new_movie_dict["Title"]} is already in our list'
-        movies.append(new_movie_dict)
 
         with open(self.file_path, "w") as writable:
-            writable.write(json.dumps(movies))
+            fieldnames = ["Title", "Rating", "Year", "Poster URL", "Note"]
+            writer = csv.DictWriter(writable, fieldnames=fieldnames)
+
+            writer.writeheader()
+            writer.writerow(new_movie_dict)
 
         message = f'Movie {new_movie_dict["Title"]} successfully added!'
         return message
 
     def delete_movie(self, title):
         # Delete movie from database(If it exists/is valid)
-        with open(self.file_path, "r") as json_movie_storage:
-            movies = json.loads(json_movie_storage.read())
+        with open(self.file_path, "r") as readable:
+            movies = csv.reader(readable)
 
         message = f"Movie {title} does not exist (In our database at-least)!"  # If movie title not in dict
 
         for movie in movies:
-            if movie["Title"] == title:
-                movies.remove(movie)  # Removes movie from list
+            if movie['Title'] == title:
+                movies = movies.drop(movies[movies.movie].index)  # Removes movie from file
                 message = f"Movie {title} successfully deleted"
 
-        with open(self.file_path, "w") as json_movie_storage:
-            json_movie_storage.write(json.dumps(movies))
+        with open(self.file_path, "w") as writable:
+            fieldnames = ["Title", "Rating", "Year", "Poster URL", "Note"]
+            writer = csv.DictWriter(writable, fieldnames=fieldnames)
+            writer.writer(movies)
 
         return message
 
     def update_movie(self, title, note):
         # Updates movies: if present in list, and if data is valid.
         with open(self.file_path, "r") as readable:
-            movies = json.loads(readable.read())
+            movies = csv.reader(readable)
 
         count = 0
-        index = -1
 
+        row = None
         for movie in movies:
             if movie['Title'] == title:
-                index = movies.index(movie)
+                row = movie
                 count += 1
             if count > 0:
                 break
@@ -82,10 +91,17 @@ class StorageJson(IStorage):
             return f'''Unfortunately "{title}" is not in our data base.
         Double check your spelling or maybe expand our list!'''
 
-        movies[index]['Note'] = note
-        message = f"Your note has been added to the movie!"
+        if row is not None:
+            row['Note'] = note
+            message = f"Your note has been added to the movie!"
 
-        with open(self.file_path, "w") as writable:
-            writable.write(json.dumps(movies))
+            with open(self.file_path, "w") as writable:
+                fieldnames = ["Title", "Rating", "Year", "Poster URL", "Note"]
+                writer = csv.DictWriter(writable, fieldnames=fieldnames)
+                writer.writer(movies)
+
+        else:
+            message = """We had an issue locating your movie,\
+                       Please ensure the movie is present in our list and its spelling is correct"""
 
         return message
